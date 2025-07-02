d865302c4b631db032ee0a85f4d832c63952da1c
ApurvaK <kulkarniapurva2003@gmail.com>
2025-07-02 13:52:50 +0530

    list of products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <>
     <Header />
-    <div className="App">
-      <h1>Welcome apu</h1>
+    <div className="m-5 py-3 ">
+      <HomeScreen />
     </div>
     <Footer />
     </>
