c73980b31d444868a3cccecc91aea03d2db67844
ApurvaK <kulkarniapurva2003@gmail.com>
2025-07-04 09:39:08 +0530

    new changes

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,21 @@
-import products from '../products';
+
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import axios from 'axios';
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(()=>{
+   const fetchProducts = async ()=>{
+    const{data}=await axios.get('/api/products');
+    setProducts(data);
+   };
+   fetchProducts();
+  },[]);
+
+
   return (
     <>
       <h1 style={{ color: '#4b0082' }}>Latest Products</h1>
